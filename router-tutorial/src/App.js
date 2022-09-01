682e386f4f6fe52eeeee78ef971d9a3e59aa14d5
kimkyutae <rbxo9085@ulalalab.com>
2022-09-02 00:46:53 +0900

    [react-router] Link컴포넌트 사용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to='/'>홈</Link>
+        </li>
+        <li>
+          <Link to='/about'>소개</Link>
+        </li>
+      </ul>
       <Routes>
         <Route path='/' element={<Home />} exact={true} />
         <Route path='/about' element={<About />} />
